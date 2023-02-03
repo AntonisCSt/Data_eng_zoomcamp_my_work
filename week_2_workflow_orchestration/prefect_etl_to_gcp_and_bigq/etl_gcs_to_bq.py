abaaca43421cb8c9317ea109eb317e8c9e90309d
AntonisCSt <a.x-stellas@hotmail.com>
2023-02-03 16:35:05 +0000

    added gcp to bq

--- week_2_workflow_orchestration/prefect_etl_to_gcp_and_bigq/etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/prefect_etl_to_gcp_and_bigq/etl_gcs_to_bq.py
@@ -5,17 +5,40 @@ from prefect import flow,task
 from prefect_gcp.cloud_storage import GcsBucket
 from prefect_gcp import GcpCredentials
 
-@task(log_prints=True)
+@task(log_prints=True,retries=2)
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
 
-    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}"
+    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("week-2-zoom")
     gcs_block.get_directory(from_path=gcs_path, local_path=f"./data/")
     print(f"path: ./data/{gcs_path}")
 
     return Path(f"./data/{gcs_path}")
 
+@task(log_prints=True)
+def transform(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    print(path)
+    df = pd.read_parquet(path)
+    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    df['passenger_count'].fillna(df['passenger_count'].mean(),inplace=True)
+    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    return df
+
+@task(log_prints=True,retries=2)
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BigQuery"""
+    gcp_credentials_block = GcpCredentials.load("zoom-service-account")
+    df.to_gbq(destination_table="trips_data_all.rides",
+        project_id="my-rides-antonis",
+        credentials= gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500000,
+        if_exists="append"
+    )
+
+
+    return
 @flow(name=" ETL-TO-BIG-QUERY")
 def etl_gcs_to_bq() -> None:
     """Main ETL flow to load data into Big Query"""
@@ -25,6 +48,7 @@ def etl_gcs_to_bq() -> None:
     month = 1
     
     path = extract_from_gcs(color, year, month)
-    
+    df = transform(path)
+    write_bq(df)
 if __name__ == '__main__':
     etl_gcs_to_bq()
