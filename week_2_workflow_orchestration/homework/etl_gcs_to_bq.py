161a6e04034df0779028872cddc3b7655da144bf
AntonisCSt <a.x-stellas@hotmail.com>
2023-02-05 16:30:55 +0000

    homework week 2

---
+++ week_2_workflow_orchestration/homework/etl_gcs_to_bq.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import pandas as pd
+
+from prefect import flow,task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+
+@task(log_prints=True,retries=2)
+def extract_from_gcs(color: str, year: int, month: int) -> Path:
+    """Download trip data from GCS"""
+
+    gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
+    gcs_block = GcsBucket.load("week-2-zoom")
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"./data/")
+    print(f"path: ./data/{gcs_path}")
+
+    return Path(f"./data/{gcs_path}")
+
+@task(log_prints=True)
+def transform(path: Path) -> pd.DataFrame:
+    """Data cleaning example"""
+    print(path)
+    df = pd.read_parquet(path)
+    print(f"number of rows: {len(df)}")
+    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    #df['passenger_count'].fillna(df['passenger_count'].mean(),inplace=True)
+    #print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    return df
+
+@task(log_prints=True,retries=2)
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BigQuery"""
+    gcp_credentials_block = GcpCredentials.load("zoom-service-account")
+    df.to_gbq(destination_table="trips_data_all.rides",
+        project_id="my-rides-antonis",
+        credentials= gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500000,
+        if_exists="append"
+    )
+    return
+
+@flow(name=" ETL-TO-BIG-QUERY")
+def etl_gcs_to_bq(color : str, year : int, month : int) -> None:
+    """Main ETL flow to load data into Big Query"""
+    
+    path = extract_from_gcs(color, year, month)
+    df = transform(path)
+    write_bq(df)
+    return
+
+@flow(name='parent flow')
+def etl_parent_flow(color : str, year : int, months : list[int])-> None:
+    """Parent flow for running multiple months"""
+    for month in months:
+        etl_gcs_to_bq(color,year,month)
+    return
+
+if __name__ == '__main__':
+    color = "yellow"
+    months = [2,3]
+    year = 2019
+    etl_parent_flow()
